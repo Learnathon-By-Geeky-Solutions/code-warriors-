95c9494b8faffc20ec124d88bed5067715cd22f8
NafisFuad106 <107258321+NafisFuadShahid@users.noreply.github.com>
2025-03-03 23:31:46 +0600

    fixed duplicate codes in multiplayer map service

--- project/backend/multiplayer-map-service/src/main/java/com/map/MetaHive/dto/CreateRoomRequest.java
+++ project/backend/multiplayer-map-service/src/main/java/com/map/MetaHive/dto/CreateRoomRequest.java
@@ -1,26 +1,11 @@
 package com.map.MetaHive.dto;
 
-public class CreateRoomRequest {
-    private String username;
-    private String roomId;
-
-    public CreateRoomRequest() {}
-
-    public CreateRoomRequest(String username, String roomId) {
-        this.username = username;
-        this.roomId = roomId;
+public class CreateRoomRequest extends RoomRequest {
+    public CreateRoomRequest() {
+        super();
     }
 
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public String getRoomId() {
-        return roomId;
-    }
-    public void setRoomId(String roomId) {
-        this.roomId = roomId;
+    public CreateRoomRequest(String roomId, String username) {
+        super(roomId, username);
     }
 }
--- project/backend/multiplayer-map-service/src/main/java/com/map/MetaHive/dto/JoinRoomRequest.java
+++ project/backend/multiplayer-map-service/src/main/java/com/map/MetaHive/dto/JoinRoomRequest.java
@@ -1,26 +1,11 @@
 package com.map.MetaHive.dto;
 
-public class JoinRoomRequest {
-    private String username;
-    private String roomId;
-
-    public JoinRoomRequest() {}
-
-    public JoinRoomRequest(String username, String roomId) {
-        this.username = username;
-        this.roomId = roomId;
+public class JoinRoomRequest extends RoomRequest {
+    public JoinRoomRequest() {
+        super();
     }
 
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public String getRoomId() {
-        return roomId;
-    }
-    public void setRoomId(String roomId) {
-        this.roomId = roomId;
+    public JoinRoomRequest(String roomId, String username) {
+        super(roomId, username);
     }
 }
