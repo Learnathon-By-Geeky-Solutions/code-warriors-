94133422687224d31b78698523dd88e2d1ff4e7b
phigratio <muqtadir.mujahid02@gmail.com>
2025-03-03 20:06:41 +0600

    fixed issuses of unused parameters

--- project/backend/doc-server/src/main/java/com/meta/doc/dtos/DocsDTO.java
+++ project/backend/doc-server/src/main/java/com/meta/doc/dtos/DocsDTO.java
@@ -1,12 +1,13 @@
 package com.meta.doc.dtos;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@Builder
 public class DocsDTO {
-
     private String id;
     private String teamId;
     private String officeId;
@@ -17,16 +18,4 @@ public class DocsDTO {
     private List<DocsDTO> children;
     private int level;
     private List<DocumentFileDTO> files;
-
-    public DocsDTO(String id, String teamId, String officeId, String title, String content, String parentId, String rootGrandparentId, int level) {
-        this.id = id;
-        this.teamId = teamId;
-        this.officeId = officeId;
-        this.title = title;
-        this.content = content;
-        this.parentId = parentId;
-        this.rootGrandparentId = rootGrandparentId;
-        this.level = level;
-    }
-    public DocsDTO() {}
 }
