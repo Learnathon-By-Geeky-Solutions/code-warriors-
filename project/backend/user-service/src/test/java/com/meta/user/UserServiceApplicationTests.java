e81933e07d3e5a6cf3473596690690aca22f1549
SaidurIUT <saidurrahman@iut-dhaka.edu>
2025-03-23 22:37:06 +0600

    fix: Adds a nested comment explaining why this method is empty

--- project/backend/user-service/src/test/java/com/meta/user/UserServiceApplicationTests.java
+++ project/backend/user-service/src/test/java/com/meta/user/UserServiceApplicationTests.java
@@ -8,6 +8,8 @@ class UserServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// This test verifies that the application context loads successfully.
+		// No assertions are needed since Spring will fail the test if the context cannot be loaded.
 	}
 
 }
